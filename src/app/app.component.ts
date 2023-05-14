184934a2113fd6380a8d53bc9c03845c535cc879
Bereke Sakan <bereke3103@mail.ru>
2023-05-14 18:53:36 +0600

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angularTutorialUdemy';
+  //!eventBindings
+  inputValue = 'inpueValue';
+  keyUpValue = 'keyUp';
+  title = 'Dynamic title';
+  img = 'https://logowik.com/content/uploads/images/react7473.logowik.com.webp';
+
+  constructor() {
+    setTimeout(() => {
+      //одностороняя связка
+      //вот здесь мы html bind-им с typescrt-ом
+      this.img =
+        'https://w7.pngwing.com/pngs/620/322/png-transparent-angularjs-ruby-on-rails-typescript-web-application-icon-hacker-angle-triangle-logo.png';
+    }, 5000);
+  }
+
+  //!eventBindings
+  onInput(event: any) {
+    console.log('event:', event);
+    this.inputValue = event.target.value;
+  }
+
+  onKeyUp(event: any) {
+    this.keyUpValue = event.target.value;
+  }
 }
