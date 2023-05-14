3b779d3b273199a3db3b9277f0beeac551e3460a
Bereke Sakan <bereke3103@mail.ru>
2023-05-14 19:02:33 +0600

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,28 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  //!eventBindings
-  inputValue = 'inpueValue';
-  keyUpValue = 'keyUp';
-  title = 'Dynamic title';
-  img = 'https://logowik.com/content/uploads/images/react7473.logowik.com.webp';
+  //Way Bindings
+  title = 'Way Bindings';
 
-  constructor() {
-    setTimeout(() => {
-      //одностороняя связка
-      //вот здесь мы html bind-им с typescrt-ом
-      this.img =
-        'https://w7.pngwing.com/pngs/620/322/png-transparent-angularjs-ruby-on-rails-typescript-web-application-icon-hacker-angle-triangle-logo.png';
-    }, 5000);
-  }
-
-  //!eventBindings
   onInput(event: any) {
-    console.log('event:', event);
-    this.inputValue = event.target.value;
-  }
-
-  onKeyUp(event: any) {
-    this.keyUpValue = event.target.value;
+    this.title = event.target.value;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { PostComponent } from './post/post.component';
 import { Post2Component } from './post2/post2.component';
 import { Post3Component } from './post3/post3.component';
 import { Post4Component } from './post4/post4.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   //Сюда мы передаем все компоненты, созданные нами
@@ -16,7 +17,11 @@ import { Post4Component } from './post4/post4.component';
     Post3Component,
     Post4Component,
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    //Way Bindings
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
