f58a2b0134d4fc6f7667bddcce9cc052e214fd87
Bereke Sakan <bereke3103@mail.ru>
2023-05-14 19:08:22 +0600

    3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  //Way Bindings
-  title = 'Way Bindings';
-
-  onInput(event: any) {
-    this.title = event.target.value;
-  }
+  toggleBackground = false;
 }
