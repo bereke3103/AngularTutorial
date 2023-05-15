d8ed8304c5e05d5d033289015f7443dd914118bf
Bereke Sakan <bereke3103@mail.ru>
2023-05-15 20:54:16 +0600

    content child для того? чтобы получить дочерний элемент

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -6,9 +12,14 @@ import { Post } from '../app.component';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   //Input -> декоратор, означающий, что он принимает от родителя (для данного случае от App.Component.ts)
+
+  @ContentChild('info', { static: true }) ElementRef: ElementRef;
 
   @Input() myPost: Post;
   constructor() {}
+  ngOnInit(): void {
+    console.log(this.ElementRef.nativeElement);
+  }
 }
